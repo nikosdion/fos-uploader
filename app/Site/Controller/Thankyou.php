a9abb7d44cee5a554aa684ea76ecdec195026e2d
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-12-25 18:25:26 +0200

    Very basic thank-you page

---
+++ app/Site/Controller/Thankyou.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package    poc-upload-to-s3
+ * @copyright  Copyright (c)2018-2018 Akeeba Ltd
+ * @license    proprietary
+ */
+
+namespace Site\Controller;
+
+use Awf\Container\Container;
+use Awf\Mvc\Controller;
+
+class Thankyou extends Controller
+{
+	public function __construct(Container $container = null)
+	{
+		$this->modelName = 'main';
+
+		parent::__construct($container);
+	}
+
+}
---
+++ app/Site/ViewTemplates/thankyou/default.blade.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package    poc-upload-to-s3
+ * @copyright  Copyright (c)2018-2018 Akeeba Ltd
+ * @license    proprietary
+ */
+
+?>
+<div class="akeeba-panel--teal">
+    <header class="akeeba-block-header">
+        <h1>@lang('SITE_THANKYOU_HEADER')</h1>
+    </header>
+    <p>
+        TODO -- Include some information here
+    </p>
+</div>
